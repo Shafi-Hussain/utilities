ec63e8d5e9fdd7a2a39e6caa4279827781eeb569
Md. Shafi Hussain <Md.Shafi.Hussain@ibm.com>
2025-09-11 09:21:23 +0530

    Create find_libs.py

---
+++ find_libs.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from json import loads
+from io import StringIO
+from pathlib import Path
+
+try:
+    from lddwrap import list_dependencies, _output_json
+except ModuleNotFoundError:
+    print("Please install pylddwrap: 'python -m pip install pylddwrap'")
+    exit(1)
+
+VENV = Path(os.path.dirname(os.path.dirname(sys.executable)))
+stream = StringIO()
+
+libs_needed = set()
+for library in VENV.rglob("*.so"):
+    # Note: try-block is needed as I noticed "debugpy" build amd64 .so files which fail with lddwrap on ppc64le
+    try:
+        deps = list_dependencies(path=library)
+    except:
+        print("FAILED:", library)
+    _output_json(deps=deps, stream=stream)
+    missing_libs = list(map(lambda dep: dep["soname"], filter(lambda dep: not dep["found"], loads(stream.getvalue()))))
+    #if missing_libs:
+    #    print(library, list(set(missing_libs)))
+    libs_needed.update(missing_libs)
+    stream.seek(0)
+    stream.truncate(0)
+
+stream.close()
+print("\n".join(sorted(libs_needed)))
